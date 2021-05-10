cc5dc8a71376df5d0c37dac0209942562c319726
sanskriti01 <80110840+sanskriti01@users.noreply.github.com>
2021-05-10 20:43:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var database;
+var canvas;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+function setup(){
+    database = firebase.database();
+    canvas = createCanvas(400,400);
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+
+function draw(){
+    if(playerCount === 4){
+        game.update(1)
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+
+}
+
+
